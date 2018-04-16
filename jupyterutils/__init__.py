edd3c4014c68afd4b453a658d708adac55948cf8
adam <athornton@gmail.com>
2018-04-16 16:47:40 -0700

    Add show_with_bokeh_server.

--- jupyterutils/__init__.py
+++ jupyterutils/__init__.py
@@ -3,5 +3,6 @@ LSST Jupyter Utilities
 """
 from jupyterutils.prepuller import Prepuller
 from jupyterutils.scanrepo import ScanRepo
+from jupyterutils.notebook import show_with_bokeh_server
 from ._version import __version__
-all = [Prepuller, ScanRepo]
+all = [Prepuller, ScanRepo, show_with_bokeh_server]
