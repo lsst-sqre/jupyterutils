230651ad35ed8aced52b9035073bf6a6cc580c27
adam <athornton@gmail.com>
2020-02-03 15:12:44 -0700

    Update workflow route and nomenclature.

--- jupyterhubutils/config/lsstconfig.py
+++ jupyterhubutils/config/lsstconfig.py
@@ -103,13 +103,13 @@ class LSSTConfig(metaclass=Singleton):
         self.api_route = os.getenv('API_ROUTE') or '/api'
         self.tap_route = os.getenv('TAP_ROUTE') or '/api/tap'
         self.soda_route = os.getenv('SODA_ROUTE') or '/api/image/soda'
-        self.workflow_route = os.getenv('WORKFLOW_ROUTE') or '/workflow'
+        self.workflow_route = os.getenv('WORKFLOW_ROUTE') or '/wf'
         self.external_firefly_url = os.getenv('EXTERNAL_FIREFLY_URL')
         self.external_js9_url = os.getenv('EXTERNAL_JS9_URL')
         self.external_api_url = os.getenv('EXTERNAL_API_URL')
         self.external_tap_url = os.getenv('EXTERNAL_TAP_URL')
         self.external_soda_url = os.getenv('EXTERNAL_SODA_URL')
-        self.external_workflow_route = os.getenv('EXTERNAL_WORKFLOW_ROUTE')
+        self.external_workflow_url = os.getenv('EXTERNAL_WORKFLOW_URL')
         self.auto_repo_urls = os.getenv('AUTO_REPO_URLS')
         # Prepuller settings
         self.lab_repo_owner = os.getenv('LAB_REPO_OWNER') or 'lsstsqre'
