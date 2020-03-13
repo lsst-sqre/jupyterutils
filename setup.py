20b233a02745e977477d7bdf8f78312f0ac6c222
adam <athornton@gmail.com>
2020-03-13 10:11:02 -0700

    Move LSST Manager initialization for spawner into __init__()

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setuptools.setup(
     keywords='lsst',
     install_requires=[
         'requests>=2.0.0,<3.0.0',
-        'kubernetes>=9.0.0',
+        'kubernetes>=10.0.0,<11.0.0',
         'semver>=2.0.0,<3.0.0',
         'oauthenticator>=0.9.0,<1.0.0',
         'jupyter-client>=5.0.0,<7.0.0',
