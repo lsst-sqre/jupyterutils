83cc4075746500e18d64953ad398003c099e5bc0
adam <athornton@gmail.com>
2020-06-02 16:14:53 -0700

    Remove Workflow Manager

--- jupyterhubutils/lsstmgr/middlemanager.py
+++ jupyterhubutils/lsstmgr/middlemanager.py
@@ -8,16 +8,15 @@ from .namespacemanager import LSSTNamespaceManager
 from .optionsformmanager import LSSTOptionsFormManager
 from .quotamanager import LSSTQuotaManager
 from .volumemanager import LSSTVolumeManager
-from .workflowmanager import LSSTWorkflowManager
 
 
 class LSSTMiddleManager(Loggable):
     '''The LSSTMiddleManager is a class that holds references to various
     LSST-specific management objects and delegates requests to them.
-    The idea is that an LSST Spawner, or an LSST Workflow Manager,
-    could instantiate a single LSSTMiddleManager, which would then be
-    empowered to perform all LSST-specific operations, reducing
-    configuration complexity.
+    The idea is that an LSST Spawner could instantiate a single
+    LSSTMiddleManager, which would then be empowered to perform all
+    LSST-specific operations, reducing configuration complexity.
+
     '''
 
     def __init__(self, *args, **kwargs):
@@ -27,6 +26,8 @@ class LSSTMiddleManager(Loggable):
         self.log.info(
             "Parent of LSST Middle Manager is '{}'".format(self.parent))
         self.authenticator = kwargs.pop('authenticator', None)
+        self.spawner = kwargs.pop('spawner', None)
+        self.user = kwargs.pop('user', None)
         self.api_mgr = LSSTAPIManager(parent=self)
         self.auth_mgr = LSSTAuthManager(parent=self)
         self.env_mgr = LSSTEnvironmentManager(parent=self)
@@ -34,10 +35,8 @@ class LSSTMiddleManager(Loggable):
         self.optionsform_mgr = LSSTOptionsFormManager(parent=self)
         self.quota_mgr = LSSTQuotaManager(parent=self)
         self.volume_mgr = LSSTVolumeManager(parent=self)
-        self.workflow_mgr = LSSTWorkflowManager(parent=self)
         self.api = self.api_mgr.api
         self.rbac_api = self.api_mgr.rbac_api
-        self.wf_api = self.api_mgr.wf_api
 
     def ensure_resources(self):
         '''Delegate to namespace manager.
@@ -50,20 +49,20 @@ class LSSTMiddleManager(Loggable):
         '''
         md = {"parent": str(self.parent),
               "authenticator": str(self.authenticator),
-              "spawner": str(self.spawner),
-              "user": str(self.user),
               "api": str(self.api),
               "rbac_api": str(self.rbac_api),
-              "wf_api": str(self.wf_api),
               "config": self.config.dump(),
               "api_mgr": self.api_mgr.dump(),
               "auth_mgr": self.auth_mgr.dump(),
               "env_mgr": self.env_mgr.dump(),
               "optionsform_mgr": self.optionsform_mgr.dump(),
               "quota_mgr": self.quota_mgr.dump(),
-              "volume_mgr": self.volume_mgr.dump(),
-              "workflow_mgr": self.workflow_mgr.dump()
+              "volume_mgr": self.volume_mgr.dump()
               }
+        if self.user:
+            md["user"] = "".format(self.user)
+        if self.spawner:
+            md["spawner"] = self.spawner.dump()
         return md
 
     def toJSON(self):
